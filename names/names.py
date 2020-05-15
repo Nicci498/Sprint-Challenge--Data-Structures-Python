c85ab687b0b54a1b5b6dc539a84e3c9be795d133
Nicole white <you@example.com>
2020-05-15 07:45:53 -1000

    pau

--- names/names.py
+++ names/names.py
@@ -1,5 +1,49 @@
 import time
 
+class BtsNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+    # Insert the given value into the tree
+    def insert(self, value):
+        # compare new node to node value
+        if value < self.value:
+            # if left doesnt exist
+            if self.left is None:
+                # create left if no val to left
+                self.left = BtsNode(value)
+            else:
+                # recurse left
+                self.left.insert(value)
+        else:
+            # value is equal or greater recurse right
+            if self.right is None:
+                # create right is no val to right
+                self.right = BtsNode(value)
+            else:
+                # recurse right
+                self.right.insert(value)
+    def contains(self, target):
+        # when we start searching, self will be the root
+        # compare the target against self
+        #
+        # Criteria for returning False: we know we need to go in one direction
+        # but there's nothing in the left or right direction
+        if target == self.value:
+            return True
+        if target < self.value:
+            # go left if left is a BinarySearchTree
+            if not self.left:
+                return False
+            return self.left.contains(target)
+        else:
+            # go right if right is a BinarySearchTree
+            if not self.right:
+                return False
+            return self.right.contains(target)
+    # Return the maximum value found in the tree
+a = BtsNode("")
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -12,11 +56,15 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# Replace the nested for loops below with your 
+bst = BtsNode(names_1[0])
+
+for name in names_1:
+    a.insert(name)
+
+for name2 in names_2:
+    if a.contains(name2):
+        duplicates.append(name2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
